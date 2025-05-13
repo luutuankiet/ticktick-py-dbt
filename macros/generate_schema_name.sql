32fc05fb5aced83219c498a421a89a1ddb7fd296
luutuankiet <luutuankiet.ftu2@gmail.com>
2025-05-13 21:17:25 +0700

    hotfix generate_schema_name

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -2,6 +2,8 @@
     {%- set default_schema = target.schema -%}
     {%- if target.name == 'ci' -%}
         {{ env_var("CI_SCHEMA",'ci') }}
+    {%- elif not custom_schema_name -%}
+        {{ default_schema }}
     {%- else -%}
         {{ default_schema }}_{{ custom_schema_name | trim }}
     {%- endif -%}
